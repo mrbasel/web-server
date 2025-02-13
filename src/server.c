e4ab590d7e28e69913632de21f10dbc79a5656a6
Basel <baselalnassr8@gmail.com>
2025-02-13 22:20:53 +0300

    implement multi-threading to handle multiple requests at a time

--- src/server.c
+++ src/server.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "server.h"
 #include "socket.h"
+#include <threads.h>
 
 #define BUFFER_SIZE 1024
 
@@ -18,28 +19,52 @@ Server* create_server(int port) {
     return server;
 }
 
+typedef struct {
+    char* buffer;
+    int socket;
+    RequestHandler handler;
+} RequestArgs;
+
+int handle_request(void* arg) {
+    RequestArgs* args = (RequestArgs*)arg;
+    char* buffer = args->buffer;
+    int socket = args->socket;
+    RequestHandler handler = args->handler;
+
+    HttpRequest* parsed_request = parse_request(buffer);
+    if (parsed_request == NULL) {
+        free(args);
+        fprintf(stderr, "could not parse request\n");
+        return 1;
+    }
+
+    HttpResponse* response = create_response(parsed_request);
+    log_http_transaction(parsed_request, response);
+    sleep(5);
+
+    handler(parsed_request, response);
+    char* serialized_response = serialize_response(response);
+    send(socket, serialized_response, strlen(serialized_response), 0);
+
+    free_request(parsed_request);
+    close(socket);
+    free(args);
+    return 0;
+}
+
 void server_listen(Server* server, RequestHandler handler) {
     char buffer[BUFFER_SIZE] = {0};
 
     int accepted_socket;
+    thrd_t t;
     while(1) {
         accepted_socket = accept_connection(server->_socket_fd, server->_socket_addr, buffer, BUFFER_SIZE);
-        HttpRequest* parsed_request = parse_request(buffer);
-        if (parsed_request == NULL) {
-            fprintf(stderr, "could not parse request\n");
-            continue;
-        }
-
-
-        HttpResponse* response = create_response(parsed_request); 
-        log_http_transaction(parsed_request, response);
-
-        handler(parsed_request, response);
-        char* serialized_response = serialize_response(response);
-        send(accepted_socket, serialized_response, strlen(serialized_response), 0);
-
-        free_request(parsed_request);
-        close(accepted_socket);
+        RequestArgs* args = malloc(sizeof(RequestArgs));
+        args->buffer = buffer;
+        args->socket = accepted_socket;
+        args->handler = handler;
+        thrd_create(&t, handle_request, args);
+        thrd_detach(t);
     }
     free_server(server);
 }
